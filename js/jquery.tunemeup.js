707197e86910e7d04b291d05fb1ed0b192fd5eeb
Dan Bolivar <danbo314@gmail.com>
2015-01-02 19:04:07 -0500

    add counter during playback

--- js/jquery.tunemeup.js
+++ js/jquery.tunemeup.js
@@ -18,26 +18,26 @@
             wavesurfer = Object.create(WaveSurfer),
             $player = $(this),
             waveId = "wave_" + settings.id,
-            repeat = false;
+            repeat = false,
+            counter = 0,
+            countPoint;
 
         if (!settings.waveColor) { settings.waveColor = themeDefaults[settings.theme].waveColor; }
 
         $player.append("<div class='tmuPlayer " + settings.theme + "'>" +
             "<div class='heydings buttonNav stop'>S</div>" +
             "<div class='heydings buttonNav playPause'>P</div>" +
-            "<div id='" + waveId + "'><div class='duration'></div></div>" +
+            "<div id='" + waveId + "'><div class='counter overlay'></div>" +
+            "<div class='duration overlay'></div></div>" +
             "<div class='heydings buttonNav repeat'>r</div>" +
             "</div>").on("click", ".playPause", function () {
-
-            if ($(this).text() === "P") {
-                $(this).text("u");
-            } else {
-                $(this).text("P");
-            }
-
             wavesurfer.playPause();
         }).on("click", ".stop", function () {
             wavesurfer.stop();
+
+            clearInterval(countPoint);
+            counter = 0;
+            $player.find(".counter").hide();
 
             $player.find(".playPause").text("P");
         }).on("click", ".repeat", function () {
@@ -77,23 +77,16 @@
         wavesurfer.load(settings.songPath);
 
         wavesurfer.on("ready", function (progress, ev) {
-            var totSecs = wavesurfer.getDuration(),
-                hours = parseInt(totSecs/3600) % 24,
-                minutes = parseInt(totSecs/60) % 60,
-                seconds = parseInt(totSecs % 60, 10),
-                dur;
+            var dur  = getDisplayFromSeconds(wavesurfer.getDuration());
 
-            hours = (hours === 0) ? "" : hours + ":";
-            minutes = (minutes === 0) ? "" : !hours ? minutes + ":" : (minutes < 10) ? "0" + minutes + ":" : minutes + ":";
-            seconds = (!hours && !minutes) ? seconds + "s" : (seconds < 10) ? "0" + seconds : seconds;
-
-            dur = hours + minutes + seconds;
-
-            $player.find(".duration").text(dur);
+            $player.find(".duration").text(dur).show();
         });
 
         wavesurfer.on("finish", function () {
             $player.find(".playPause").text("P");
+
+            clearInterval(countPoint);
+            counter = 0;
 
             if (repeat) {
                 setTimeout(function () {
@@ -104,10 +97,84 @@
 
         wavesurfer.on("play", function () {
             $player.find(".playPause").text("u");
+
+            countPoint = startCounterFrom(counter, $player.find(".counter"));
+        });
+
+        wavesurfer.on("pause", function () {
+            $player.find(".playPause").text("P");
+
+            clearInterval(countPoint);
+            counter = getSecondsFromDisplay($player.find(".counter").text());
+        });
+
+        wavesurfer.on("seek", function (progress) {
+            var secs = wavesurfer.getDuration() * progress;
+
+            clearInterval(countPoint);
+            counter = secs;
+
+            countPoint = startCounterFrom(secs, $player.find(".counter"));
         });
 
         return this;
 
     };
+
+    function startCounterFrom(startInt, $node) {
+        var count = startInt;
+
+        $node.text(getDisplayFromSeconds(count)).show();
+
+        return setInterval(function () {
+            $node.text(getDisplayFromSeconds(++count));
+        }, 1000);
+    }
+
+    function getDisplayFromSeconds(totSecs) {
+        var hours = parseInt(totSecs/3600) % 24,
+            minutes = parseInt(totSecs/60) % 60,
+            seconds = parseInt(totSecs % 60, 10);
+
+        if (hours === 0) {
+            hours = "";
+            minutes += ":";
+        }
+        else {
+            hours += ":";
+            minutes = (minutes < 10) ? "0" + minutes : minutes;
+            minutes += ":";
+        }
+
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return hours + minutes + seconds;
+    }
+
+    function getSecondsFromDisplay(dispString) {
+        var parts = dispString.split(":"),
+            hours,
+            minutes,
+            seconds,
+            res = 0;
+
+        if (parts.length === 3) {
+            hours = parseInt(parts[0]);
+            minutes = parseInt(parts[1]);
+            seconds = parseInt(parts[2]);
+        }
+        else {
+            minutes = parseInt(parts[0]);
+            seconds = parseInt(parts[1])
+        }
+
+        if (hours) {
+            res += (hours * 3600);
+        }
+
+        res += (minutes * 60 + seconds);
+
+        return res;
+    }
 
 }(jQuery));
